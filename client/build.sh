cd297c28a7fdea60a37b2eed11bf1e432f62aac1
fragger42 <58386643+fragger42@users.noreply.github.com>
2022-12-16 00:52:15 +0100

    Add script files for build and start

---
+++ client/build.sh
@@ -0,0 +1,6 @@
+export VUE_APP_BACKEND_SCHEME=http
+export VUE_APP_BACKEND_HOST=192.168.178.2
+export VUE_APP_BACKEND_PORT=5000
+export BASE_URL=/Toniebox
+export NODE_OPTIONS=--openssl-legacy-provider
+npm run build
---
+++ client/start.sh
@@ -0,0 +1,5 @@
+export VUE_APP_BACKEND_SCHEME=http
+export VUE_APP_BACKEND_HOST=192.168.178.2
+export VUE_APP_BACKEND_PORT=5000
+export NODE_OPTIONS=--openssl-legacy-provider
+npm run serve -- --port 3000
