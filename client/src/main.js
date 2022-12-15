adb34ae06a275fe99f32dd8766b22845ab4abab0
fragger42 <58386643+fragger42@users.noreply.github.com>
2022-12-16 00:55:54 +0100

    Update to Vue v3

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import Vue from 'vue';
+import * as Vue from 'vue';
+import * as VueRouter from 'vue-router';
+import AudioBooks from './components/AudioBooks.vue';
 import App from './App.vue';
-import router from './router';
 
-Vue.config.productionTip = false;
+const routes = [
+  {
+    path: '/',
+    name: 'AudioBooks',
+    component: AudioBooks,
+  },
+];
 
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount('#app');
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+const app = Vue.createApp(App);
+app.config.globalProperties.$backendUrl = `${process.env.VUE_APP_BACKEND_SCHEME}://${process.env.VUE_APP_BACKEND_HOST}:${process.env.VUE_APP_BACKEND_PORT}`;
+app.use(router).mount('#app');
